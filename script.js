e5f500d1f306b49cc72e64b4f9a2cb8b54155eac
iridiumquartz <courtneycavanah@gmail.com>
2025-04-19 16:31:30 -0400

    Update script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+(function() {
+    var gc = document.createElement("script");
+    gc.setAttribute("data-goatcounter", "https://chromonospace.goatcounter.com/count");
+    gc.async = true;
+    gc.src = "//gc.zgo.at/count.js";
+    document.body.appendChild(gc);
+  })();
