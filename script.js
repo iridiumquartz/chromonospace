4f90bfa4020779f0ce816e93e7da3542e3601c72
iridiumquartz <courtneycavanah@gmail.com>
2025-08-04 17:33:34 -0400

    Update script.js

--- script.js
+++ script.js
@@ -9,10 +9,11 @@
 class NavComponent extends HTMLElement {
   connectedCallback() {
     this.innerHTML = `
-        <a href="profile.html">profile</a>
-        <a href="journal/index.html">journal</a>
+        <a href="/profile.html">profile</a>
+        <a href="/journal/index.html">journal</a>
     `;
   }
 }
 
 customElements.define('nav-component', NavComponent);
+
