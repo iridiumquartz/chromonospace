0c481278ffc1c95452f5afac44b77702d5198bbc
iridiumquartz <courtneycavanah@gmail.com>
2025-08-04 17:26:08 -0400

    Update script.js

--- script.js
+++ script.js
@@ -5,3 +5,14 @@
     gc.src = "//gc.zgo.at/count.js";
     document.body.appendChild(gc);
   })();
+
+class NavComponent extends HTMLElement {
+  connectedCallback() {
+    this.innerHTML = `
+        <a href="profile.html">profile</a>
+        <a href="journal/index.html">journal</a>
+    `;
+  }
+}
+
+customElements.define('nav-component', NavComponent);
